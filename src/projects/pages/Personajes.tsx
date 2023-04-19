6e7eed0e3729919b101f41dbc79f35e1f36fa5e1
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-04-19 16:36:01 -0600

    grid de personajes configurado

--- src/projects/pages/Personajes.tsx
+++ src/projects/pages/Personajes.tsx
@@ -1,6 +1,31 @@
+import { useState, useEffect, useCallback} from 'react';
+import {Container} from '@mui/material';
+import ArrayCharacters from '../classes/Array/ArrayCharacter';
+
 const Personajes = () => {
+
+    const [ personajes, setPersonajes ] = useState<ArrayCharacters>(new ArrayCharacters);
+    const [ page, setPage ] = useState<number>(1);
+
+    useEffect( () => {
+        const fetchPersonajes = async() => {
+            try{
+                const arrayPersonajes = await ArrayCharacters.getCharacters({page});
+                setPersonajes(arrayPersonajes);
+            }catch(error){
+                console.log(error);
+            } 
+        }
+
+        fetchPersonajes();
+
+    }, [page])
+
+    console.log(personajes);
     return ( 
-        <h1>Personajes</h1>
+        <Container fixed sx={{ border: '1px solid black', marginTop: '1rem'}} >
+            hola
+        </Container>
      );
 }
  
