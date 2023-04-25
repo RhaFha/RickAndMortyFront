e5ca3a5fa6309dbea1d4778e1ba8bfc35b9c255d
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-04-25 15:15:31 -0600

    eliminando comnentarios

--- src/components/CardEpisodio.tsx
+++ src/components/CardEpisodio.tsx
@@ -17,18 +17,9 @@ const CardEpisodio: React.FC<IPropsEpisodio> = ({episodio}) => {
                     <Typography variant="h5" component="div" sx={{ height: 65 }}>
                     {name}
                     </Typography>
-                    {/* <Typography  color="text.secondary">
-                    Type:
-                    </Typography> */}
                     <Typography variant="body2">
                     {episode} - {air_date}
                     </Typography>
-                    {/* <Typography  color="text.secondary">
-                    Dimension:
-                    </Typography> */}
-                    {/* <Typography sx={{ mb: 1.5 }} variant="body2">
-                    {dimension}
-                    </Typography> */}
                 </CardContent>
                 <CardActions>
                     <Button size="small">Learn More</Button>
